843835cecb95243403b019a78c9ee36baf6c2c24
simonk <sek_99@hotmail.co.uk>
2018-06-06 19:24:03 +0100

    implement Spring @ContextConfiguration and @Configuration

--- ui-acceptance-tests/src/test/java/io/github/symonk/cucumber/GeneralHooks.java
+++ ui-acceptance-tests/src/test/java/io/github/symonk/cucumber/GeneralHooks.java
@@ -1,8 +1,11 @@
 package io.github.symonk.cucumber;
 
+import io.github.symonk.spring.SpringConfiguration;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.test.context.ContextConfiguration;
 
 @Slf4j
+@ContextConfiguration(classes = SpringConfiguration.class)
 public class GeneralHooks {
 
 
--- ui-acceptance-tests/src/test/java/io/github/symonk/spring/SpringConfiguration.java
+++ ui-acceptance-tests/src/test/java/io/github/symonk/spring/SpringConfiguration.java
@@ -1,13 +1,18 @@
 package io.github.symonk.spring;
 
 import io.github.symonk.webdriver.Driver;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 
+@Slf4j
+@Configuration
 public class SpringConfiguration {
 
     @Bean
     public Driver driver() {
-        return new Driver();
+        return new Driver(null);
     }
 
 }
